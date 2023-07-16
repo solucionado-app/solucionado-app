bfc21b77985237fedf3693f6f53b6183da54e2cd
santiagoavilez <santiagolingreed@gmail.com>
2023-07-15 21:54:27 -0300

    Sign out button added

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({ children }: React.PropsWithChildren<object>
     return (
         <>
             <Nav />
-            <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">{children}</main>
+            <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gradient-to-b from-[#2e026d] to-[#15162c]">{children}</main>
         </>
     );
 }
