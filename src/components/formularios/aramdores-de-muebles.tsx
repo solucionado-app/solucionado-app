5efd1de86d2ad56566205fa6a62a36492d95a4c1
santiagoavilez <santiagolingreed@gmail.com>
2023-07-21 12:08:26 -0300

    fix armadores de muebles

--- src/components/formularios/aramdores-de-muebles.tsx
+++ src/components/formularios/aramdores-de-muebles.tsx
@@ -39,45 +39,45 @@ export function ArmadoresDeMueblesForm() {
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
                 <FormField
-                control={form.control}
-                name="elementosPintura"
-                render={({ field }) => (
-                    <FormItem className="space-y-3">
-                        <FormLabel>¿Tiene herramientas necesarias para armar lo que solicita?</FormLabel>
-                        <FormControl>
-                            <RadioGroup
-                            onValueChange={field.onChange}
-                            defaultValue={field.value}
-                            className="flex flex-col space-y-1"
-                            >
-                                <FormItem className="flex items-center space-x-3 space-y-0">
-                                    <FormControl>
-                                        <RadioGroupItem value="Si" />
-                                    </FormControl>
-                                    <FormLabel className="font-normal">
-                                        Si
-                                    </FormLabel>
-                                </FormItem>
-                                <FormItem className="flex items-center space-x-3 space-y-0">
-                                    <FormControl>
-                                        <RadioGroupItem value="No" />
-                                    </FormControl>
-                                    <FormLabel className="font-normal">
-                                        No
-                                    </FormLabel>
-                                </FormItem>
-                            </RadioGroup>
-                        </FormControl>
-                        <FormMessage />
-                        <FormDescription>
+                    control={form.control}
+                    name="elementosPintura"
+                    render={({ field }) => (
+                        <FormItem className="space-y-3">
+                            <FormLabel>¿Tiene herramientas necesarias para armar lo que solicita?</FormLabel>
+                            <FormControl>
+                                <RadioGroup
+                                    onValueChange={field.onChange}
+                                    defaultValue={field.value}
+                                    className="flex flex-col space-y-1"
+                                >
+                                    <FormItem className="flex items-center space-x-3 space-y-0">
+                                        <FormControl>
+                                            <RadioGroupItem value="Si" />
+                                        </FormControl>
+                                        <FormLabel className="font-normal">
+                                            Si
+                                        </FormLabel>
+                                    </FormItem>
+                                    <FormItem className="flex items-center space-x-3 space-y-0">
+                                        <FormControl>
+                                            <RadioGroupItem value="No" />
+                                        </FormControl>
+                                        <FormLabel className="font-normal">
+                                            No
+                                        </FormLabel>
+                                    </FormItem>
+                                </RadioGroup>
+                            </FormControl>
+                            <FormMessage />
                             <div className="w-[465px] text-sm bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
                                 role="alert">
                                 <p className="font-bold">Nota</p>
-                                <p>Este trabajo se cotizará en base al elemento a armar.</p>
+                                <FormDescription>
+                                    Este trabajo se cotizará en base al elemento a armar.
+                                </FormDescription>
                             </div>
-                        </FormDescription>
-                    </FormItem>
-                )}
+                        </FormItem>
+                    )}
                 />
 
                 <Button type="submit">Cotizar</Button>
