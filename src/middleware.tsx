fe9df88d66ab85187554623622066c8259452616
santiagoavilez <santiagolingreed@gmail.com>
2023-07-22 14:04:16 -0300

    fix middleware auth

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,20 +1,11 @@
 import { redirectToSignIn } from '@clerk/nextjs';
 import { authMiddleware } from '@clerk/nextjs/server';
 export default authMiddleware({
-    afterAuth(auth, req) {
-        // handle users who aren't authenticated
-        if (!auth.userId && !auth.isPublicRoute) {
-            return redirectToSignIn({ returnBackUrl: req?.url }) as never;
-        }
-
-        return null;
-    },
     publicRoutes: ["/", "/contacto", "/login", "/registro", "/(solucionar)(.*)", "/(api|trpc)(.*)"],
 });
 
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-
 
 };
