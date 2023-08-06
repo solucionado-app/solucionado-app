47c41597d6551806497cd7871ca30882bff7005a
santiagoavilez <santiagolingreed@gmail.com>
2023-08-06 11:54:04 -0300

    fix trpc console.log

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -10,7 +10,7 @@ export default createNextApiHandler({
   onError:
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
-        // console.error(
+         console.error(
         `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
           );
         }
