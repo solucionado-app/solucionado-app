55b8069fb2e57318c9b29be7f5283a2ba415e4ce
santiagoavilez <santiagolingreed@gmail.com>
2023-09-26 17:41:22 -0300

    fix mail url

--- src/components/budgets/AlertMercadoPagoIntegrate.tsx
+++ src/components/budgets/AlertMercadoPagoIntegrate.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '../ui/alert-dialog'
-import { Button } from '../ui/button'
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from '../ui/alert-dialog'
 import { useUser } from '@clerk/nextjs'
 
 interface Props {
