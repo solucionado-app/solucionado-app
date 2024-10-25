a275762e691850fe7938e53b05fcef78c8691241
santiagoavilez <santiagolingreed@gmail.com>
2024-10-25 15:29:55 -0300

    fix: Update authMiddleware, delete afterauth() fuction

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,6 @@
 import {  authMiddleware } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
 export default authMiddleware({
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    afterAuth:  (auth, req, evt) => {
-
-        if (!auth.isPublicRoute){
-            if (auth.userId) {
-              return NextResponse.next();
-            }
-            const url = new URL(`/login?redirect=${req.nextUrl.pathname}`, req.url);
-            return NextResponse.redirect(url)
-        }
-        if(req.nextUrl.pathname === '/registro/usuario' || req.nextUrl.pathname === '/registro/solucionador'  ){
-            return NextResponse.next()
-        }
-
-    },
     publicRoutes: ["/", "/contacto", '/(login)(.*)','/(registro)(.*)','/registro/usuario','/registro/solucionador/', "/(solucionar/)(.*)", "/(api|trpc)(.*)", "/api/webhooks/mercadopago/autorization"],
     ignoredRoutes: ["/api/webhooks/mercadopago/autorization", "/api/webhooks/user"],
 });
